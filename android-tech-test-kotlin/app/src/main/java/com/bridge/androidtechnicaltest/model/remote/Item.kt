c6205a189f554944360a247049bc76e1df9ff32b
uduak umanah <uduakumanah10@gmail.com>
2025-06-17 06:00:27 +0100

    check

---
+++ android-tech-test-kotlin/app/src/main/java/com/bridge/androidtechnicaltest/model/remote/Item.kt
@@ -0,0 +1,10 @@
+package com.bridge.androidtechnicaltest.model.remote
+
+data class Item(
+    val country: String,
+    val image: String,
+    val latitude: Double,
+    val longitude: Double,
+    val name: String,
+    val pupilId: Int
+)
